fb51dd0f76d4ff5bd26f6eed9454f0d68820c6d4
felixmiranda <felixmiranda.net@gmail.com>
2021-05-17 00:05:54 -0500

    Add project files.

---
+++ EjemploApiRest.Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using EjemploApiRest.Abstractions;
+using System;
+
+namespace EjemploApiRest.Repository
+{
+    public interface IRepository<T> : ICrud<T>
+    {
+
+    }
+
+    public class Repository<T> : IRepository<T>
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Collections.Generic.IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Save(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
